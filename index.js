e1bb160aa47e5a32072b05fa8ce0d6fbca0ee880
Landon Folkers <32934735+landonfolkers@users.noreply.github.com>
2018-04-16 09:00:48 -0600

    added missing data

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ let PORT = process.env.PORT || 3000
 
 app.use(cors())
 
-app.get('/', function(req, res) {
-    res.json({data})
+app.get('/', function (req, res) {
+    res.json({ data })
 })
 
 function findId(data, id) {
-    for(let i = 0; i < data.length; i++) {
-        if (data[i].id == id){
-        return data[i]
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id == id) {
+            return data[i]
         }
     }
 }
 
-app.get('/:id', function(req, res) {
+app.get('/:id', function (req, res) {
     let item = findId(data, req.params.id)
     if (!item) {
-        return res.status(404).json( {
+        return res.status(404).json({
             error: {
                 message: "No record found!"
             }
         })
     } else {
-        return res.json({data: item})
+        return res.json({ data: item })
     }
 })
 
--- instructors.js
+++ instructors.js
@@ -9,6 +9,26 @@ module.exports = [{
     title: "Lead Instructor",
     numberOfDogs: 0
 },{
+    id: 3,
+    fullName: "CJ Reynolds",
+    title: "Lead Instructor",
+    numberOfDogs: 0
+},{
+    id: 4,
+    fullName: "Brooks Patton",
+    title: "Lead Instructor",
+    numberOfDogs: 0
+},{
+    id: 5,
+    fullName: "Roberto Ortega",
+    title: "Lead Instructor",
+    numberOfDogs: 1
+},{
+    id: 6,
+    fullName: "Chad Drummond",
+    title: "Instructor",
+    numberOfDogs: 0
+},{
     id: 7,
     fullName: "Kim Schlesinger",
     title: "Instructor",
@@ -19,8 +39,24 @@ module.exports = [{
     title: "Associate Instructor",
     numberOfDogs: 1
 },{
+    id: 9,
+    fullName: "Cass Torske",
+    title: "Resident",
+    numberOfDogs: 1
+},{
     id: 10,
     fullName: "Matt Winzer",
     title: "Resident",
     numberOfDogs: 2
+},{
+    id: 11,
+    fullName: "Aaron Goodman",
+    title: "Resident",
+    numberOfDogs: 0
+},{
+    id: 12,
+    fullName: "Michelle Bergquist",
+    title: "Resident",
+    numberOfDogs: 1
+
 }];
